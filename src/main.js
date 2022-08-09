62f6a7f6d71e295ca1e192a0ae5090a2b5b10af6
b1ackc4t <b1ackc4t@qq.com>
2022-08-09 17:00:47 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import {createApp} from 'vue'
 import store from '@/store'
 import App from './App.vue'
 import router from './router/index.js'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import './assets/css/material-dashboard.min.css'
-
+import 'element-plus/dist/index.css'
 import VMdEditor from '@kangc/v-md-editor';
 import '@kangc/v-md-editor/lib/style/base-editor.css';
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
@@ -24,7 +22,7 @@ function getServerConfig() {  // 定义
         const server = process.env.NODE_ENV !== 'development' ? res.data.BASE_URL_PROD : res.data.BASE_URL_DEV
         requests.defaults.baseURL = server
         app.provide('server', server)
-        app.use(ElementPlus).use(store).use(router).use(VMdEditor).mount('#app')
+        app.use(store).use(router).use(VMdEditor).mount('#app')
     })
 }
 
